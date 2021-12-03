cae6dba03a31b0903e1f8c0ab70fdc5b15ac4f12
Irina Khaustova <Irina170580@yandex.ru>
2021-12-03 23:32:27 +0300

    version2

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -6,24 +6,23 @@ const decrease = function() {
         clearInterval(countdown);
     }
 };
-//const countdown = setInterval(decrease1, 1000);
-countdown;
-let fgh = timerValue.textContent
+//const countdown = setInterval(decrease, 1000);
+//countdown;
+let fgh = (timerValue.textContent)*1000
 
 
 const decrease1 = function() {
-    let timer1 = new Date(fgh*1000);
-hours = timer1.getUTSHours();
-minutes = timer1.getUTSMinutes();
-seconds = timer1.getSeconds();
-    fgh -= 1000;
-    timerValue.textContent = hours.toString().padStart(2, '0') + ':' + 
-    minutes.toString().padStart(2, '0') + ':' + 
-    seconds.toString().padStart(2, '0');
-    if (fgh < 0) {
-        alert('Вы победили в конкурсе!');
-        clearInterval(countdown);
-    }
+
+  let timer1 = new Date(fgh);
+  hours = timer1.getUTSHours();
+  minutes = timer1.getUTSMinutes();
+  seconds = timer1.getSeconds();
+  fgh -= 1000;
+  timerValue.textContent = hours.toString().padStart(2, '0') + ':' + minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
+  if (fgh < 0) {
+    alert('Вы победили в конкурсе!');
+    clearInterval(countdown);
+  }
 }
 
 const countdown = setInterval(decrease1, 1000);
